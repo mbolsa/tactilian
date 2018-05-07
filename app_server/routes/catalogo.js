6700626cc5d629dd700306b17a8e7f6c512eb981
jorgeaguaron <jorge@aguaron.com>
2018-05-07 17:38:10 +0200

    Catalogo

--- app_server/routes/catalogo.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrlCatalogo = require('../controllers/catalogo');
-
-router.get('/', ctrlCatalogo.catalogo);
-module.exports = router;
